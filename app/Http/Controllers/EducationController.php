0535a7212bef96df0d27dc41356f1b4853884dd0
Atomsuperza1234 <atomsuperza2@gmail.com>
2017-08-05 22:49:57 +0700

    first commit

---
+++ app/Http/Controllers/EducationController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Education
+
+class EducationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $education = Education::get();
+      return view('education.index', compact('education'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('education.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $education = new Education ($request->all());
+      $education->save();
+      return redirect()->route('education.index')->with('alert-succress','Add new leave type success.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $education = Education::find($id);
+      $education->delete();
+      session()->flash('message','Delete Successfully');
+      return redirect('/education');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyData</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!-- Styles -->
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+          <div class="container-fluid">
+            <br><br>
+            <div class="row">
+              <div class="col-md-12">
+                <center>
+                <img src="/uploads/avatars/default.jpg" class="img-thumbnail" style="width:160px; height:160px;">
+              </center>
+              </div>
+            </div>
+            <br>
+            <br>
+            <div class="row">
+              <div class="col-md-4">
+                <div class="panel panel-info" >
+                  <div class="panel-heading"><h4><b>Personal detail</b></h4></div>
+                  <div class="panel-body" >
+                    <label for="birthday" class="col-md-4 control-label" >Name :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">Jirayu Chinpongsuewan</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >Birth day :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">1993 December 28</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >Gender :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">male</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >Nationality :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">Thai</label><br>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="panel panel-info" >
+                  <div class="panel-heading"><h4><b>Contact.</b></h4></div>
+                  <div class="panel-body" >
+                    <label for="birthday" class="col-md-4 control-label" >Country :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">Thailand</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >City :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">Chiang mai</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >Apartment :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">2205 Phudaw Place 63/18 M.8 Suthep 50200</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >Tel. :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">+66882600565</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >email. :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">atomdeveloper@gmail.com</label>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="panel panel-info" >
+                  <div class="panel-heading"><h4><b>Graduate.</b></h4></div>
+                  <div class="panel-body" >
+                    <label for="birthday" class="col-md-4 control-label" >University :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">Chiang Mai University, THAILAND</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >Faculty :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">CAMT : College of Arts, Media and Technology</label><br>
+                    <label for="birthday" class="col-md-4 control-label" >Section :</label>
+                    <label for="birthday" class="col-md-8 control-label" align="left">SE : software Engineer</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="row">
+              <div class="col-md-12">
+                    <center>
+                      <img src="/uploads/skill/skill.png" >
+                    </center>
+              </div>
+            </div>
+            <br>
+            <div class="row">
+              <div class="col-md-2">
+
+              </div>
+              <div class="col-md-8">
+                <div class="panel panel-success" >
+                  <div class="panel-heading"><h4><b>Experience.</b></h4></div>
+                  <div class="panel-body" >
+                    <a href="https://github.com/atomsuperza2/NCT127-HRM" class="col-md-4 control-label">Project HR : Human Resource</a>
+                    <label for="detailHr" class="col-md-8 control-label" align="left">////////////Put Detail///////////</label><br>
+                    <a href="https://github.com/atomsuperza2/E-Office" class="col-md-4 control-label">Project E-office </a>
+                    <label for="detailHr" class="col-md-8 control-label" align="left">////////////Put Detail///////////</label><br>
+                    <a href="https://github.com/atomsuperza2/Diabetes1" class="col-md-4 control-label">Project Diabetes Care </a>
+                    <label for="detailHr" class="col-md-8 control-label" align="left">The web application project develop by using .net MVC framwork C# language. Diabetes Care develop for support and care diabetes patient.</label><br>
+                    <a href="https://github.com/khessarin/i-am-a-neard" class="col-md-4 control-label">Project Wad Chaimongkol  </a>
+                    <label for="detailHr" class="col-md-8 control-label" align="left">The web application project develop by using .net MVC framwork C# language. Wad Chaimongkol develop for presentation about Wad Chaimongkol.</label><br>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-2">
+
+              </div>
+            </div>
+          </div>
+        </div>
+    </body>
+</html>

b4b4869a1bf05e32791ed178ca75e658311dd569
Atomsuperza1234 <atomsuperza2@gmail.com>
2017-08-08 08:14:44 +0700

    commit

--- resources/views/my_data.blade.php
+++ resources/views/my_data.blade.php
@@ -4,9 +4,20 @@
     <meta charset="utf-8">
     <title>My Data</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <style type="text/css">
-      body {font-family: "Lato", sans-serif;}
+      body {
+        font-family: "Lato", sans-serif;
+        background-color: #FFF;
+      }
       /* Style the links inside the sidenav */
       #mySidenav a {
         position: absolute; /* Position them relative to the browser window */
@@ -28,41 +39,58 @@
       #content-ifo {
         top: 70px;
         text-align: center;
-        background-color: #4CAF50;
+        background-color: #FF8533;
       }
 
       #content-skill {
         top: 130px;
         text-align: center;
-        background-color: #2196F3; /* Blue */
+        background-color: #FF8C40; /* Blue */
       }
 
       #content-project {
         top: 190px;
         text-align: center;
-        background-color: #f44336; /* Red */
+        background-color: #FF944D; /* Red */
       }
 
       #content-contact {
         top: 250px;
         text-align: center;
-        background-color: #555 /* Light Black */
+        background-color: #FF9C59 /* Light Black */
       }
 /*//////////////////////contentbox//////////////////*/
       div.contentbox {
         background: #FFF;
+        color: white;
         height: 625px;
-        margin: 20px;
+        margin: 45px;
         font-size: 28px;
         text-align: center;
-        border: #CCC 1px dashed;
+        /*border: #CCC 1px dashed;*/ /* เส้นปะ */
+        border-radius: 15px;
+
       }
+      #content2 {
+        background: #FF7D26;
+        border-radius: 15px;
+      }
+      #content3 {
+        background: #FF7D26;
+        border-radius: 15px;
+      }
+      #content4 {
+        height: 250px;
+        background: #FF7D26;
+        border-radius: 15px;
+      }
+
 /*//////////////////////button on top//////////////////*/
       #myBtn {
         display: none;
         position: fixed;
-        bottom: 20px;
-        right: 40px;
+        bottom: 55px;
+        right: 65px;
         z-index: 99;
         border: none;
         outline: none;
@@ -77,12 +105,12 @@
         background-color: #555;
       }
 /*//////////////////////3D card//////////////////*/
-      .flip3D{ width:280px; height:250px; margin:20px; margin-top: 100px; float:left; }
+      .flip3D{ width:250px; height:250px; margin:30px; margin-top: 100px; float:left; }
       .flip3D > .front{
 	       position:absolute;
 	       -webkit-transform: perspective( 600px ) rotateY( 0deg );
 	       transform: perspective( 600px ) rotateY( 0deg );
-	       background:#FC0; width:280px; height:250px; border-radius: 7px;
+	       background:#FC0; width:250px; height:250px; border-radius: 7px;
 	       -webkit-backface-visibility: hidden;
 	       backface-visibility: hidden;
 	       transition: -webkit-transform .5s linear 0s;
@@ -92,7 +120,7 @@
 	        position:absolute;
 	        -webkit-transform: perspective( 600px ) rotateY( 180deg );
 	        transform: perspective( 600px ) rotateY( 180deg );
-	        background: #80BFFF; width:280px; height:250px; border-radius: 7px;
+	        background: #595959; width:250px; height:250px; border-radius: 7px;
 	        -webkit-backface-visibility: hidden;
 	        backface-visibility: hidden;
 	        transition: -webkit-transform .5s linear 0s;
@@ -107,7 +135,7 @@
 	        transform: perspective( 600px ) rotateY( 0deg );
         }
         /*//////////////////////3D card small//////////////////*/
-              .flip3D2{ width:80px; height:80px; margin:10px; margin-top: 5px; float:left; }
+              .flip3D2{ width:80px; height:80px; margin:10px; margin-top: 5px; margin-left: 20px; float:left; }
               .flip3D2 > .front{
         	       position:absolute;
         	       -webkit-transform: perspective( 600px ) rotateY( 0deg );
@@ -118,11 +146,13 @@
         	       transition: -webkit-transform .5s linear 0s;
         	       transition: transform .5s linear 0s;
                       }
+
+
               .flip3D2 > .back{
         	        position:absolute;
         	        -webkit-transform: perspective( 600px ) rotateY( 180deg );
         	        transform: perspective( 600px ) rotateY( 180deg );
-        	        background: #80BFFF; width:80px; height:80px; border-radius: 7px;
+        	        background: #595959; width:80px; height:80px; border-radius: 7px;
         	        -webkit-backface-visibility: hidden;
         	        backface-visibility: hidden;
         	        transition: -webkit-transform .5s linear 0s;
@@ -148,32 +178,54 @@
         font-size: 17px;
         width: 50%;
 
+
       }
 
       .tablink:hover {
         background-color: #777;
       }
 
+      #defaultOpen {border-bottom-left-radius: 15px;}
+      #OtherDetailb {border-bottom-right-radius: 15px;}
 /* Style the tab content */
       .tabcontent {
         color: white;
         display: none;
-        padding: 234px;
+        height: 550px;
         text-align: center;
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
       }
-      #PeronalDetail {background-color:red;}
-      #OtherDetail {background-color:green;}
+      #OtherDetail {background-color:#FF7D26;}
+      #PeronalDetail {background-color:#FF7D26;}
 
+      div .content-pd1 {
+        margin-top: 220px;
+        margin-left: 220px;
+        text-align: center;
+        font-size: 40px;
+        width: 200px;
+        height: auto;
+        border: #CCC 10px dashed;
+      }
+      div .content-pd2 {
+        margin-top: 180px;
+        margin-left: 100px;
+        text-align: left;
+        font-size: 20px;
+        width: 500px;
+        height: auto;
+        /*border: #CCC 1px dashed;*/
+      }
   /*//////////////////////icon social media//////////////////*/
       .fa {
-        padding: 50px;
-        font-size: 50px;
-        width: 50px;
+        padding: 35px;
+        font-size: 35px;
+        width: 35px;
         text-align: center;
         text-decoration: none;
-        margin-left: 210px;
-        margin-top: 245px;
-
+        margin-left: 30px;
+        margin-top: 50px;
         border-radius: 50%;
       }
 
@@ -198,13 +250,15 @@
       }
 /*//////////////////////conten skill//////////////////*/
       .content-skill {
-        background: #FFF;
+        background: #FF7D26;
         height: 90px;
-        margin: 10px;
+        margin: 15px;
         font-size: 24px;
-        text-align: left;
-        border: #CCC 1px dashed;
+        /*text-align: right;*/
+        /*border: #CCC 1px dashed;*/
+        border-radius: 15px;
       }
+
       </style>
 
       <script type="text/javascript">
@@ -263,14 +317,28 @@
     <div id="content1" class="contentbox">
 
       <div id="PeronalDetail" class="tabcontent">
-        <h3>Peronal Detail</h3>
+        <div class="row">
+          <div class="col-md-5">
+            <div class="content-pd1">
+              Jirayu C.
+            </div>
+          </div>
+          <div class="col-md-7">
+            <div class="content-pd2">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Being the developer, programmer, and even software engineer at the same time seems to be overkill for a single person.
+              Handling task that required multitasking of both creativity and logical to achieve a goal cannot be done practically.
+              It required dedication, patient, and responsibility to sustain the quality and quantity to the very end.
+            </div>
+          </div>
+        </div>
       </div>
 
       <div id="OtherDetail" class="tabcontent">
         <h3>Other Detail</h3>
       </div>
-      <button class="tablink" onclick="openInfo('PeronalDetail', this, 'red')" id="defaultOpen">Peronal Detail</button>
-      <button class="tablink" onclick="openInfo('OtherDetail', this, 'green')">Other Detail</button>
+      <button class="tablink" onclick="openInfo('PeronalDetail', this, '#FF7D26')" id="defaultOpen">Peronal Detail</button>
+      <button class="tablink" onclick="openInfo('OtherDetail', this, '#FF7D26')" id="OtherDetailb">Other Detail</button>
       <script>
       function openInfo(info,elmnt,color) {
           var i, tabcontent, tablinks;
@@ -293,102 +361,161 @@
 <!-- ///////////////////////////////content Programming Experiences/////////////////////////////// -->
     <div id="content2" class="contentbox">
       <h3>Programming Experiences</h3>
-        <div id="content-skill-1" class="content-skill">Language
-          <div class="flip3D2">
-            <div class="back">Box 1 - Back</div>
-            <div class="front">Box 1 - Front</div>
-          </div>
-          <div class="flip3D2">
-            <div class="back">Box 2 - Back</div>
-            <div class="front">Box 2 - Front</div>
-          </div>
-          <div class="flip3D2">
-            <div class="back">Box 3 - Back</div>
-            <div class="front">Box 3 - Front</div>
-          </div>
-          <div class="flip3D2">
-            <div class="back">Box 4 - Back</div>
-            <div class="front">Box 4 - Front</div>
-          </div>
-        </div>
-        <div id="content-skill-2" class="content-skill">Interest in mobile application
-          <div class="flip3D2">
-            <div class="back">Box 1 - Back</div>
-            <div class="front">Box 1 - Front</div>
-          </div>
-        </div>
-        <div id="content-skill-3" class="content-skill">Interest in backend
-          <div class="flip3D2">
-            <div class="back">Box 1 - Back</div>
-            <div class="front">Box 1 - Front</div>
-          </div>
-          <div class="flip3D2">
-            <div class="back">Box 2 - Back</div>
-            <div class="front">Box 2 - Front</div>
-          </div>
-          <div class="flip3D2">
-            <div class="back">Box 3 - Back</div>
-            <div class="front">Box 3 - Front</div>
-          </div>
-          <div class="flip3D2">
-            <div class="back">Box 4 - Back</div>
-            <div class="front">Box 4 - Front</div>
+        <div id="content-skill-1" class="content-skill">
+          <div class="row">
+            <div class="col-md-4">
+              <p style="text-align: right">Language</p>
+            </div>
+            <div class="col-md-8">
+              <div class="flip3D2">
+                <div class="back"> 85% </div>
+                <div class="front"> <img src="/uploads/C.png" style="margin-top:5px; width:70px; height:70px;"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> 80% </div>
+                <div class="front"> <img src="/uploads/php.png" style="margin-top:5px; width:70px; height:70px;"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> 60% </div>
+                <div class="front"> <img src="/uploads/java.png" style="margin-top:5px; width:70px; height:70px;"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> 20% </div>
+                <div class="front"> <img src="/uploads/js.png" style="margin-top:5px; width:70px; height:70px;"> </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div id="content-skill-4" class="content-skill">Interest in frontend
-          <div class="flip3D2">
-            <div class="back">Box 1 - Back</div>
-            <div class="front">Box 1 - Front</div>
-          </div>
-          <div class="flip3D2">
-            <div class="back">Box 2 - Back</div>
-            <div class="front">Box 2 - Front</div>
-          </div>
-          <div class="flip3D2">
-            <div class="back">Box 3 - Back</div>
-            <div class="front">Box 3 - Front</div>
+
+        <div id="content-skill-2" class="content-skill">
+          <div class="row">
+            <div class="col-md-4">
+              <p style="text-align: right">Interest in backend</p>
+
+            </div>
+            <div class="col-md-8">
+              <div class="flip3D2">
+                <div class="back"> 80% </div>
+                <div class="front"><img src="/uploads/lara.png" style="margin-top:5px; width:70px; height:70px;"></div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> 80%</div>
+                <div class="front"><img src="/uploads/net1.png" style="margin-top:5px; width:70px; height:70px;"></div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"> </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div id="content-skill-5" class="content-skill">system admin
-          <div class="flip3D2">
-            <div class="back">Box 1 - Back</div>
-            <div class="front">Box 1 - Front</div>
+        <div id="content-skill-3" class="content-skill">
+          <div class="row">
+            <div class="col-md-4">
+              <p style="text-align: right">Interest in frontend</p>
+
+            </div>
+            <div class="col-md-8">
+              <div class="flip3D2">
+                <div class="back"></div>
+                <div class="front"><img src="/uploads/jq.gif" style="margin-top:5px; width:70px; height:70px;"></div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"><img src="/uploads/b.png" style="margin-top:5px; width:70px; height:70px;"></div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"><img src="/uploads/sl.jpg" style="margin-top:5px; width:70px; height:70px;"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"> </div>
+              </div>
+            </div>
           </div>
-          <div class="flip3D2">
-            <div class="back">Box 2 - Back</div>
-            <div class="front">Box 2 - Front</div>
+        </div>
+        <div id="content-skill-4" class="content-skill">
+          <div class="row">
+            <div class="col-md-4">
+              <p style="text-align: right">system admin</p>
+            </div>
+            <div class="col-md-8">
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"><img src="/uploads/my.png" style="margin-top:5px; width:70px; height:70px;"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"> <img src="/uploads/ms.png" style="margin-top:5px; width:70px; height:70px;"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"><img src="/uploads/psql.png" style="margin-top:5px; width:70px; height:70px;"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back">  </div>
+                <div class="front">  </div>
+              </div>
+            </div>
           </div>
-          <div class="flip3D2">
-            <div class="back">Box 3 - Back</div>
-            <div class="front">Box 3 - Front</div>
+        </div>
+        <div id="content-skill-5" class="content-skill">
+          <div class="row">
+            <div class="col-md-4">
+                <p style="text-align: right">Interest in mobile application</p>
+            </div>
+            <div class="col-md-8">
+              <div class="flip3D2">
+                <div class="back"> 50% </div>
+                <div class="front"> <img src="/uploads/as.png" style="margin-top:5px; width:70px; height:70px;">  </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"> </div>
+              </div>
+              <div class="flip3D2">
+                <div class="back"> </div>
+                <div class="front"> </div>
+              </div>
+            </div>
           </div>
         </div>
     </div>
+
 <!-- ///////////////////////////////Projects/////////////////////////////// -->
     <div id="content3" class="contentbox">
       <h3>My Project</h3>
       <div class="flip3D">
-        <div class="back">Box 1 - Back</div>
-        <div class="front">Box 1 - Front</div>
+        <div class="back"> </div>
+        <div class="front"> </div>
       </div>
       <div class="flip3D">
-        <div class="back">Box 2 - Back</div>
-        <div class="front">Box 2 - Front</div>
+        <div class="back"> </div>
+        <div class="front"> </div>
       </div>
       <div class="flip3D">
-        <div class="back">Box 3 - Back</div>
-        <div class="front">Box 3 - Front</div>
+        <div class="back"> </div>
+        <div class="front"> </div>
       </div>
       <div class="flip3D">
-        <div class="back">Box 4 - Back</div>
-        <div class="front">Box 4 - Front</div>
+        <div class="back"> </div>
+        <div class="front"> </div>
       </div>
     </div>
 <!-- ///////////////////////////////content Contact/////////////////////////////// -->
     <div id="content4" class="contentbox">
-      <a href="#" class="fa fa-facebook"></a>
+      <a href="https://www.facebook.com/jirayu.atommik" class="fa fa-facebook"></a>
       <a href="#" class="fa fa-slack"></a>
-      <a href="#" class="fa fa-github"></a>
+      <a href="https://github.com/atomsuperza2" class="fa fa-github"></a>
     </div>
   </body>
 </html>

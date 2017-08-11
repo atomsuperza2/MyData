f4f98437bd898fa3df08954efa031682aae7d944
Atomsuperza1234 <atomsuperza2@gmail.com>
2017-08-11 08:41:05 +0700

    update

--- resources/views/my_data.blade.php
+++ resources/views/my_data.blade.php
@@ -483,19 +483,19 @@
             </div>
             <div class="col-md-8">
               <div class="flip3D2">
-                <div class="back"> 75% </div>
+                <div class="back">  </div>
                 <div class="front"> <img src="/uploads/C.png" style="margin-top:5px; width:70px; height:70px;"> </div>
               </div>
               <div class="flip3D2">
-                <div class="back"> 80% </div>
+                <div class="back">  </div>
                 <div class="front"> <img src="/uploads/php.png" style="margin-top:5px; width:70px; height:70px;"> </div>
               </div>
               <div class="flip3D2">
-                <div class="back"> 60% </div>
+                <div class="back">  </div>
                 <div class="front"> <img src="/uploads/java.png" style="margin-top:5px; width:70px; height:70px;"> </div>
               </div>
               <div class="flip3D2">
-                <div class="back"> 20% </div>
+                <div class="back">  </div>
                 <div class="front"> <img src="/uploads/js.png" style="margin-top:5px; width:70px; height:70px;"> </div>
               </div>
               <div class="flip3D2">
